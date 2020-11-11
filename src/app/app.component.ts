ea430c0a22a643d7b62bcf9fa5dcbabb3c7398d5
Genadijus Paleckis <genadijus.paleckis@roxorgaming.com>
2020-11-11 22:30:07 +0000

    update root selector following tslint rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pp-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
